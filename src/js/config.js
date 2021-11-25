0e83b28f2abb761e9aa7adf57e0e32692f160091
swathisural <swathisural@gmail.com>
2021-11-25 17:58:54 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f5026a0e-4624-4b29-895e-989f0dbc037e';
+export const MODAL_CLOSE_SEC = 2.5;
